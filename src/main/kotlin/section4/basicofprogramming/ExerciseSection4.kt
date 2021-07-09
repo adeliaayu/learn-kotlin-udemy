050093ff398dc9287e9b73fb1c77d0ea8e93e9eb
adelia ayu <55054703+adeliaayu@users.noreply.github.com>
2021-07-09 15:04:02 +0700

    init project

---
+++ src/main/kotlin/section4/basicofprogramming/ExerciseSection4.kt
@@ -0,0 +1,4 @@
+package section4.basicofprogramming
+
+class ExerciseSection4 {
+}
---
+++ src/main/kotlin/section4/basicofprogramming/PracticeSection4.kt
@@ -0,0 +1,29 @@
+package section4.basicofprogramming
+
+class PracticeSection4 {
+    fun main() {
+        println( 5 / 2 )
+
+        /*Shift operation in programming shifted its decimal number in binary then turn it back to decimal number
+        shifted left by 1 means dikali 2
+        shifted left by 2 means dikali 4
+
+        shifted right by 1 means dibagi 2
+        shifted right by 2 means dibagi 4
+         */
+
+        // This is shift operation left, so in binary its shifted to the left by 2
+        println( 25 shl 2 )
+
+        // This is shift operation right, so in binary its shifted to the right by 2
+        println( 37 shr 2 )
+    }
+}
+
+/*
+This is a comment
+
+Multiline comment is here
+ */
+
+//This is also a comment
