a4d12f4e73fc245cb79f99218177b3ed612e7cf5
adelia ayu <55054703+adeliaayu@users.noreply.github.com>
2021-07-09 16:00:10 +0700

    add practice and exercise section 4 basic of programming

--- src/main/kotlin/section4/basicofprogramming/ExerciseSection4.kt
+++ src/main/kotlin/section4/basicofprogramming/ExerciseSection4.kt
@@ -1,4 +1,32 @@
 package section4.basicofprogramming
 
-class ExerciseSection4 {
+fun main () {
+
+    val myComputerRAM: Int = 12
+    val numbers = listOf(10,20)
+    println(numbers.average())
+    var number: Int = 10
+    number += 2
+    println(number)
+    number -= 10
+    println(number)
+    number *= 9
+    println(number)
+    number /= 4
+    println(number)
+    number = number shr 3
+    println(number)
+    number++
+    println(number)
+    number *= 2
+    println(number)
+    number = number shl 4
+    println(number)
 }
+
+/*
+    1. Buat constant dengan type Int kemudian inisialisasi, gunakan real-world constant
+    2. Calculate average of 2 numbers
+    3. Buat variable dengan type Int dengan value apapun kemudian lakukan operasi sebagai berikut
+        a. number +2 -10 *9 /4 shr3 +1 *2 shl4
+     */
--- src/main/kotlin/section4/basicofprogramming/PracticeSection4.kt
+++ src/main/kotlin/section4/basicofprogramming/PracticeSection4.kt
@@ -1,24 +1,50 @@
 package section4.basicofprogramming
 
-class PracticeSection4 {
-    fun main() {
-        println( 5 / 2 )
+const val myName: String = "Adelia Ayu Trilestari"
+//const val myNickname: String  error karena wajib di initialized
 
-        /*Shift operation in programming shifted its decimal number in binary then turn it back to decimal number
-        shifted left by 1 means dikali 2
-        shifted left by 2 means dikali 4
+fun main() {
+    println(5 / 2)
 
-        shifted right by 1 means dibagi 2
-        shifted right by 2 means dibagi 4
-         */
+    // This is shift operation left, so in binary its shifted to the left by 2
+    println(25 shl 2)
 
-        // This is shift operation left, so in binary its shifted to the left by 2
-        println( 25 shl 2 )
+    // This is shift operation right, so in binary its shifted to the right by 2
+    println(37 shr 2)
 
-        // This is shift operation right, so in binary its shifted to the right by 2
-        println( 37 shr 2 )
-    }
+    val myLuckyNumber: Int //Tidak perlu inisialisasi langsung
+    // type val tidak bisa diganti isinya
+    val myPiNumber: Double = 3.14
+    println(myPiNumber)
+
+    val priceNumber: Float = 2.1F
+    println(priceNumber)
+
+    var myAge: Int = 24
+    println(myAge)
+    myAge = 25
+    println(myAge)
+
+    var myBankAcc: Int = 1_000_000
+    println(myBankAcc)
+
+    myAge += 25
+    println(myAge)
+
+    myAge -= 25
+    println(myAge)
+
+    myAge--
+    println(myAge)
 }
+
+/*Shift operation in programming shifted its decimal number in binary then turn it back to decimal number
+shifted left by 1 means dikali 2
+shifted left by 2 means dikali 4
+
+shifted right by 1 means dibagi 2
+shifted right by 2 means dibagi 4
+ */
 
 /*
 This is a comment
