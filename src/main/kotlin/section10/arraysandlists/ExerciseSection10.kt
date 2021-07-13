2a4e687e06cf5b7a96c57ee7b468380c83ecb18b
adelia ayu <55054703+adeliaayu@users.noreply.github.com>
2021-07-13 20:59:08 +0700

    add exercise and practice section 10 Arrays and Lists

--- src/main/kotlin/section10/arraysandlists/ExerciseSection10.kt
+++ src/main/kotlin/section10/arraysandlists/ExerciseSection10.kt
@@ -1,5 +1,7 @@
 package section10.arraysandlists
 
+import java.util.*
+
 /*
     1. Create 5 Integer Arrays
     2. Create 5 String Arrays
@@ -20,5 +22,106 @@ package section10.arraysandlists
  */
 
 fun main(){
+    //1
+    var evenNumber = intArrayOf(2, 4, 6, 8)
+    var oddNumber: IntArray = intArrayOf(1, 3, 5, 7)
+    var number1to5: IntArray = intArrayOf(1,2,3,4,5)
+    var defaultNumber2 = IntArray(5, {2})
+    var zeroNumbers = IntArray(4)
 
+    //2
+    var cars = arrayOf("Toyota", "Avanza", "Agya")
+    var motorcycle = arrayOf("Nex", "Beat", "Vario")
+    var fruits = Array(5, {"Fruits"})
+    var rujak: Array<String> = arrayOf("Apel", "Mangga", "Timun")
+    var handphone: Array<String> = arrayOf("Samsung", "iPhone", "Xiaomi")
+
+    //3
+    var number1to10 = arrayOf(1,2,3,4,5,6,7,8,9,10).toIntArray()
+
+    //4
+    var evenDoubleNumber = listOf(2.6, 4.4, 8.6)
+    var oddDoubleNumber: List<Double> = listOf(5.3, 1.5, 7.9)
+    var randomNumbers: List<Double> = listOf(6.4, 8.0, 5.2)
+    var emptyDoubleList: List<Double> = listOf()
+    var number1toRandom = listOf<Double>(1.0, 2.0, 3.0)
+
+    //5
+    var nameList = mutableListOf("Ahmad", "Doni", "Budi", "Tony", "Karin", "Ani")
+    var gradeList: List<Int> = mutableListOf(4, 6, 9, 8)
+    var floatList: List<Float> = mutableListOf(5.6F, 3.7F, 44.5F)
+    var trueAndFalseList: List<Boolean> = mutableListOf(true, false, false)
+    var convertedMutableList: List<Int> = listOf(4, 6, 7, 5).toMutableList()
+
+    //6
+    println(nameList.get(0))
+    println(gradeList.first())
+    println(floatList.last())
+
+    //7
+    println(trueAndFalseList.isEmpty())
+    println(convertedMutableList.size)
+    println(number1toRandom.lastIndex)
+    println(randomNumbers.indexOf(8.0))
+
+    //8
+    println(nameList[1])
+    println(gradeList[0])
+    println(randomNumbers[2])
+
+    //9
+    var firstTwoNames = nameList.slice(0..1)
+    println(firstTwoNames)
+    var nextThreeNames = nameList.slice(2..4)
+    println(nextThreeNames.joinToString())
+
+    //10
+    doesThisElementExist("Ahmad", nameList)
+
+    //11
+    nameList.add("Bella")
+    println(nameList.joinToString())
+    nameList += "Valcon"
+    println(nameList.joinToString())
+
+    //12
+    nameList.add(3, "Tomoko")
+    println(nameList.joinToString())
+
+    //13
+    nameList.removeAt(5)
+    println(nameList.joinToString())
+    nameList.remove("Bella")
+    println(nameList.joinToString())
+
+    //14
+    nameList.set(3, "Jono")
+    println(nameList.joinToString())
+    nameList[0] = "Argon"
+    println(nameList.joinToString())
+
+    //15
+    for (name in nameList){
+        println(name)
+    }
+    for ((index,element) in gradeList.withIndex()){
+        println("Pada indeks ke $index berisi $element")
+    }
+
+    //16
+    var grade : Array<Int?> = arrayOf(4, null, 5, 8, 3)
+    println(grade.joinToString())
+
+    var age : List<Int>? = mutableListOf(7, 8, 34, 33)
+    println(age!!.joinToString())
+    age = null
+    println(age)
+    
+    var car : List<String?>? = mutableListOf("Agya", "Ignis", "Inova", null)
+    println(car?.joinToString())
+}
+
+// 10
+fun doesThisElementExist(name: String, array: MutableList<String>) : Boolean{
+    return (name in array)
 }
--- src/main/kotlin/section10/arraysandlists/PracticeSection10.kt
+++ src/main/kotlin/section10/arraysandlists/PracticeSection10.kt
@@ -1,5 +1,202 @@
 package section10.arraysandlists
 
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 fun main(){
 
+    // Arrays in Kotlin
+    var animalNames: Array<String> = arrayOf("Lion", "Elephant", "Squid")
+    println(animalNames.get(0))
+
+    var oddNumber: Array<Int> = arrayOf(1,3,5,7)
+    println(oddNumber.get(1))
+
+    var lions = Array(5, {"Lion"}) // kurung kurawal itu akan menjadi default value dari array
+    println(lions.get(4))
+    //println(lions.get(5)) // ERROR karena index terakhir adalah size-1
+
+    val iphone = arrayOf("iphone 4", "iphone 5") // Array dapat dibuat menggunakan constant (val) apabila tidak akan diubah valuenya
+
+    // Arrays of Primitive Type
+    var oddNumber1 = intArrayOf(1,3,5,7) // Digunakan untuk optimize proses dari baris kode, primitive type, bukan object
+    println(oddNumber1)
+
+    var prices = doubleArrayOf(3.4, 5.3, 3.5)
+    println(prices.get(0))
+
+    floatArrayOf()
+    booleanArrayOf()
+    byteArrayOf()
+    shortArrayOf()
+
+    var fiveZeroes = IntArray(8) // Default value nya adalah Int yakni angka 0 tanpa perlu didefinisikan
+    println(fiveZeroes.get(4))
+
+    // Convert object array to primitive array
+    var evenNumber = arrayOf(2, 4, 6, 8).toIntArray()
+
+    // List in Kotlin
+    /*
+    List in an interface
+    1. Concrete realization in ArrayList
+    2. Arrays > Lists
+    3. Dynamically-sized
+     */
+
+    // Immutable List
+    val fruitList = listOf("Apple", "Orange", "Cucumber")
+    val fruitArrayList = arrayListOf("Apple", "Orange", "Cucumber")
+
+    println(fruitList.get(0))
+    println(fruitArrayList.get(1))
+
+    val emptyList: List<Int> = listOf()
+    val emptyArrayList: ArrayList<Double> = arrayListOf()
+
+    // Mutable List
+    val fruitMutableList = mutableListOf("Apple", "Orange", "Cucumber")
+    val fruitMutableArrayList = arrayListOf("Apple", "Orange", "Cucumber") // Array List selalu mutable
+    val emptyMutableList: List<Int> = mutableListOf()
+
+    // Properties and Method in Kotlin
+    val users = mutableListOf("Andy", "Gary", "Patrick")
+
+    if (users.isEmpty()){ // isEmpty() is a method
+        println("List is empty. Sorry")
+    } else {
+        println("List is not empty. Continue")
+    }
+
+    if (users.size > 5){ // size is a properties
+        println("Continue")
+    } else {
+        println("Sorry")
+    }
+
+    println(users.first()) // first() is a method
+    println(users.last()) // first() is a method
+
+    // println(users.min()) DEPRECATED
+    // println(users.max()) DEPRECATED
+
+    val userAge = listOf(11, 22, 33)
+
+    // println(userAge.min()) DEPRECATED
+    // println(userAge.max()) DEPRECATED
+
+    // Indexing
+    val animals = arrayOf("Lions", "Tiger", "Leopard", "Frog", "Cat", "Dolphin", "Fox")
+
+    println(animals[2])
+
+    // Join To String
+    println(animals.joinToString())
+
+    // Slice by Ranges
+    val firstTwoAnimal = animals.slice(0..1)
+    println(firstTwoAnimal.joinToString())
+
+    val nextTwoAnimal = animals.slice(2..3)
+    println(nextTwoAnimal.joinToString())
+
+    // Checking for an Array Element
+    println(isAnimalRemoved("Tiger", animals)) // Using function
+    println(doesAnimalExist("Tiger", animals))
+
+    if (animals.contains("Lions")){  // Using contains
+        println("Lions exist")
+    } else {
+        println("Lions doesn't exist")
+    }
+
+    if (animals.slice(1..3).contains("Frog")){ // Using contains with slice
+        println("Frog does exist")
+    } else {
+        println("Frog does not exist")
+    }
+
+    // Appending Elements
+    val fruits = mutableListOf("Strawberry", "Melon", "Apple", "Orange", "Watermelon", "Banana")
+
+    fruits.add("Cucumber") // ADD elements to list
+    println(fruits.last())
+
+    fruits += "Durian"  // menggunakan operator +=
+    println(fruits.last())
+    println(fruits.joinToString())
+
+    var evenNumber1 = arrayOf(2, 4, 6, 8) // harus menggunakan variable (var) bukan constant (val)
+    println(evenNumber1.joinToString())
+
+    evenNumber1 += 10 // sebenarnya bukan menambahkan value kedalam array yang ada, tetapi membuat array baru dalam variable yang ada
+    println(evenNumber1.joinToString())
+
+    // Inserting Elements
+    fruits.add(2, "Srikaya")    // menggunakan add(idx, elm)
+    println(fruits.get(2))
+    println(fruits.joinToString())
+
+    // Removing Elements
+    fruits.remove("Melon")  // mennggunakan remove
+    println(fruits.joinToString())
+
+    var melonRemoved = fruits.remove("Watermelon")
+    println(melonRemoved)
+    println(fruits.joinToString())
+
+    fruits.removeAt(0)  // menggunakan removeAt
+    println(fruits.joinToString())
+
+    var fruitIndex1 = fruits.removeAt(1)
+    println(fruits.joinToString())
+    println("$fruitIndex1 is removed from list")
+
+    // Updating Elements
+    fruits.set(0, "Pineapple")  // menggunakan SET
+    println(fruits.joinToString())
+
+    fruits[1] = "Cranberrys"    // menggunakan index, untuk mengupdate ARRAY bisa pake index
+    println(fruits.joinToString())
+
+    fruits.sort()   // akan diurutkan sesuai abjad, menggunakan SORT
+    println(fruits.joinToString())
+
+    // Iterating over a List
+    for (fruit in fruits){  // gapake index
+        println(fruit)
+    }
+    for ((index, fruit) in fruits.withIndex()) {    // dengan index
+        println("$index, $fruit")
+    }
+
+    println(sumResult(userAge)) // menggunakan function
+
+    // Nullability and Collection types
+    var seaAnimal : List<String>? = mutableListOf("Dolphin", "Whale", "Shark")     // Nullable List
+    seaAnimal = null
+
+    var landAnimal : List<String?> = mutableListOf("Elephant", null, "Deer")     // Nullable Elements
+
+    var airAnimal : List<String?>? = mutableListOf("Hawk", "Eagle", null)       // Nullable List and Elements
+    airAnimal = null
+
+
+}
+
+// Checking for an Array Element
+fun isAnimalRemoved(animal: String, array: Array<String>) : Boolean {
+    return (animal !in array)
+}
+fun doesAnimalExist(animal: String, array: Array<String>) : Boolean {
+    return animal in array
+}
+
+// Iterating over a List
+fun sumResult(numbers: List<Int>) : Int{
+    var result = 0
+
+    for (number in numbers){
+        result += number
+    }
+    return result
 }
